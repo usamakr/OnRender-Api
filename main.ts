e08b17eb3563ddba614ced9a8d06561d083ac2ee
Usama Rashad <usamakr@gmail.com>
2023-12-20 22:07:41 +0500

    Update
    Ready to run on onrender with default get endpoint "/"

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-console.log("API run");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Server running on On Render." });
+});
+
+app.listen(9000, () => {
+  console.log(`Server started on port 9000.`);
+});
