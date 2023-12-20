c8e7c340f94fe19f1e6ca2cbfa3926ff3d754f70
Usama Rashad <usamakr@gmail.com>
2023-12-20 23:23:00 +0500

    Updated packages for Typescript

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("API run");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var app = (0, express_1.default)();
+app.get("/", function (req, res) {
+    return res.status(200).json({ message: "Server running on On Render." });
+});
+app.listen(9000, function () {
+    console.log("Server started on port 9000.");
+});
